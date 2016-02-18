58f756775b6563c453723da94f1e8fb6d4f39f01
Peter Philips <peter@planet.io>
2016-02-18 15:50:28 -0800

    fix for html safe joining

--- app/helpers/bootstrap/nav_helper.rb
+++ app/helpers/bootstrap/nav_helper.rb
@@ -57,7 +57,7 @@ module Bootstrap::NavHelper
     with_environment = options.delete(:with_environment)
     if with_environment && Rails.env != 'production'
       if text.present?
-        text = "#{text} - #{Rails.env}"
+        text = safe_join(["#{text}", "- #{Rails.env}"])
       else
         text = Rails.env
       end
